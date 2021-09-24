14e71c229613fdedbab6f3187025dc21c62d990e
peterooo94 <pietraspiotr94@gmail.com>
2021-09-24 16:37:28 +0200

    fixed mixed content error and homepage added

--- src/req/omdbReq.js
+++ src/req/omdbReq.js
@@ -1,9 +1,9 @@
 export const omdbFindByTitle = async (title) => {
-    const respond = await fetch(`http://www.omdbapi.com/?s=${title}&apikey=83b9aeb1`)
+    const respond = await fetch(`https://www.omdbapi.com/?s=${title}&apikey=83b9aeb1`)
     return respond.json()
   }
 
 export const omdbFindById = async (imdbID) => {
-    const respond = await fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=83b9aeb1`)
+    const respond = await fetch(`https://www.omdbapi.com/?i=${imdbID}&apikey=83b9aeb1`)
     return respond.json()
   }
